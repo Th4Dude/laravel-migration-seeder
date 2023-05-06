0be75b042a1c393eaad54838b5cbc55ea6c2b872
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-06 13:14:59 +0300

    foreach trains details

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
-use App\Models\Train;
+use App\Models\train;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
 
 class PageController extends Controller
 {
     public function index(){
 
-        $trains = Train::all();
+        $trains = train::all();
+        $todaysTrains = $this->getTodaysTrains();
 
-        return view('home', compact('Trains'));
+        return view('home', compact('trains','todaysTrains'));
+      }
+      
+      private function getTodaysTrains()
+      {
+          return Train::whereDate('departure_time', Carbon::today())->get();
       }
 }
+
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,40 @@
          @vite('resources/js/app.js')
     </head>
     <body>
-        <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
-        </div>
+
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">ID TRENO</th>
+                <th scope="col">COMPAGNIA</th>
+                <th scope="col">STAZIONE DI PARTENZA</th>
+                <th scope="col">STAZIONE DI ARRIVO</th>
+                <th scope="col">ORARIO PARTENZA</th>
+                <th scope="col">ORARIO ARRIVO</th>
+                <th scope="col">CODICE TRENO</th>
+                <th scope="col">NUMERO CARROZZA</th>
+                <th scope="col">PUNTUALE</th>
+                <th scope="col">CANCELLATO</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                <tr>
+                    <th scope="row">{{$train['id']}}</th>
+                    <td>{{$train['company']}}</td>
+                    <td>{{$train['departure_station']}}</td>
+                    <td>{{$train['arrival_station']}}</td>
+                    <td>{{$train['departure_time']}}</td>
+                    <td>{{$train['arrival_time']}}</td>
+                    <td>{{$train['train_code']}}</td>
+                    <td>{{$train['carriages_number']}}</td>
+                    <td>{{$train['on_time']== 0 ? 'no':'yes'}}</td>
+                    <td>{{$train['cancelled']== 0 ? 'no':'yes'}}</td>
+                  </tr>
+                @endforeach
+             
+            </tbody>
+          </table>
 
     </body>
 </html>
